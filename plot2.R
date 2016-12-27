fb37fb9e6aca5f0d025eb0c3ceecf8d87deef0b3
jaspreet1121 <jaspreet1141@hotmail.com>
2016-12-27 16:29:20 +0530

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(lubridate)
+
+a<-read.table("household_power_consumption.txt",sep = ";",header = TRUE)
+b<-filter(a,Date=="1/2/2007"|Date=="2/2/2007")
+x<-dmy_hms(paste(a$Date,a$Time))
+png("plot2.png",height=480,width=480)
+plot(x,as.numeric(paste(b$Global_active_power)),type = "l",ylab = "Global Active Power (kilowatts)",xlab = "")
+dev.off()
