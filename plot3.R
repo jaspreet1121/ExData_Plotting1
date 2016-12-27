31fd5544fc72d1c2dff3b59b4c808c95e2fb874a
jaspreet1121 <jaspreet1141@hotmail.com>
2016-12-27 16:49:37 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(lubridate)
+
+a<-read.table("household_power_consumption.txt",sep = ";",header = TRUE)
+b<-filter(a,Date=="1/2/2007"|Date=="2/2/2007")
+x<-dmy_hms(paste(a$Date,a$Time))
+png("plot3.png",height = 480.,width = 480)
+plot(x,as.numeric(paste(b$Sub_metering_1)),type = "l",xlab = "",ylab = "Energy sub metering")
+lines(x,as.numeric(paste(b$Sub_metering_2)),col = "red")
+lines(x,as.numeric(paste(b$Sub_metering_3)),col = "blue")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch = "-",col = c("black","red","blue"))
+dev.off()
